dbf9a1f1b0a167f58b09638ad28dc1ca832d6942
Piotr Krol <krolpiotr225@gmail.com>
2022-05-19 20:33:25 +0200

    get layers form AutoCad DataBase

--- ControlsDemo/ControlsDemoUntil.cs
+++ ControlsDemo/ControlsDemoUntil.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Runtime;
+using System.Collections;
 
 namespace ControlsDemo
 {
@@ -11,6 +12,25 @@ namespace ControlsDemo
         {
             MainForm mf = new MainForm();
             mf.Show();
+        }
+
+        public ArrayList GetLeyers()
+        {
+            ArrayList layers = new ArrayList();
+
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                LayerTable lyTab = trans.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
+                foreach(var ly in lyTab)
+                {
+                    LayerTableRecord layer = trans.GetObject(ly, OpenMode.ForRead)as LayerTableRecord;
+                    layers.Add(layer.Name);
+                }
+            }
+            return layers;
         }
     }
 }
